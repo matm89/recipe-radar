c9198c1d419485dcb177b098ae9651e5f391b811
matm89 <55142684+matm89@users.noreply.github.com>
2025-11-13 18:34:42 +0100

    test1

--- server/test/server.test.ts
+++ server/test/server.test.ts
@@ -15,7 +15,7 @@ afterEach(() => {
 
 test('Server connected', async ({ expect }) => {
   const response = await request(server).get('/');
-
+  //! test
   expect(response.statusCode).toBe(200);
   expect(response.body).toBe('It is alive! 🧟');
   server.close();
