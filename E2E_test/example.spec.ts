cadeea039ebaf7275e489df08fa889b18db23c4c
matm89 <55142684+matm89@users.noreply.github.com>
2025-11-18 14:58:12 +0100

    test(e2e): completed some end to end test ToDo Db Favorites
    The test is working still not working the favorite part of the test

--- E2E_test/example.spec.ts
+++ E2E_test/example.spec.ts
@@ -1,4 +1,7 @@
 import { test, expect } from '@playwright/test';
+import { mockTomatoDataRecipes } from './mocks/mockTomatoDataRecipes';
+import { mockGreenTomatoSalad } from './mocks/mockGreenTomatoSalad';
+import fs from 'fs';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:5173/');
@@ -9,8 +12,21 @@ test('has title', async ({ page }) => {
 
 //* Test homepage. Working searchbar and popular recipes
 test.describe('Homepage', () => {
-  test('SearchBar search tomato recipes', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
+    await page.route('http://localhost:3000/recipes/random', async route => {
+      const json = mockTomatoDataRecipes;
+      await route.fulfill({json});
+    });
+    
+    await page.route('http://localhost:3000/recipes?ingredients=tomato', async route => {
+      const json = mockTomatoDataRecipes;
+      await route.fulfill({json});
+    });
+    
     await page.goto('http://localhost:5173/');
+  });
+
+  test('SearchBar search tomato recipes', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Search recipes...' }).click();
     await page.getByRole('textbox', { name: 'Search recipes...' }).fill('tomato');
     await page.getByRole('button', { name: 'Search' }).click();
@@ -18,27 +34,42 @@ test.describe('Homepage', () => {
   });
 
   test('Popular recipes', async ({ page }) => {
-    await page.goto('http://localhost:5173/');
     await expect(page.getByRole('heading', { name: 'Popular Recipes' })).toBeVisible();
-    await expect(page.getByText('Popular RecipesWatching What')).toBeVisible();
   });
 });
 
 //* Test favourites functionality
 test.describe('Favourites', ()=>{
-  test('Add favourite recipe', async ({ page }) => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.route('http://localhost:3000/recipes/random', async route => {
+      const json = mockTomatoDataRecipes;
+      await route.fulfill({json});
+    });
+    
+    await page.route('http://localhost:3000/recipe/645555', async route => {
+      const json = mockGreenTomatoSalad;
+      await route.fulfill({json});
+    });
+    
+    if (fs.existsSync('database.sqlite')) {
+     await fs.unlinkSync('database.sqlite');
+    }
+
     await page.goto('http://localhost:5173/');
+  });
+
+
+
+  test('Add favourite recipe', async ({ page }) => {
     await page.getByRole('link', { name: 'Favorite Recipes' }).click();
     await expect(page.getByRole('heading', { name: 'Favorite Recipes' })).toBeVisible();
+    await expect(page.getByText('You don\'t have any favorite')).toBeVisible();
     await page.getByRole('link', { name: 'reciepe-radar-logo Recipe' }).click();
-    await page.getByRole('textbox', { name: 'Search recipes...' }).click();
-    await page.getByRole('textbox', { name: 'Search recipes...' }).fill('Turkey Pot Pie');
-    await page.getByRole('button', { name: 'Search' }).click();
-    await expect(page.getByRole('heading', { name: 'Shredded Roast Beef Stuffed' })).toBeVisible();
-    await page.getByRole('heading', { name: 'Shredded Roast Beef Stuffed' }).click();
+    await page.getByRole('heading', { name: 'Green Tomato Salad' }).click();
     await page.locator('svg').nth(2).click();
     await page.getByRole('link', { name: 'Favorite Recipes' }).click();
-    await expect(page.getByRole('heading', { name: 'Shredded Roast Beef Stuffed' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Green Tomato Salad' })).toBeVisible();
   });
   //! The API requested payment. We should never use the real API!
 });
---
+++ E2E_test/mocks/mockGreenTomatoSalad.ts
@@ -0,0 +1,59 @@
+export const mockGreenTomatoSalad = {
+    "id": 645555,
+    "title": "Green Tomato Salad",
+    "image": "https://img.spoonacular.com/recipes/645555-312x231.jpg",
+    "imageType": "jpg",
+    "usedIngredientCount": 1,
+    "missedIngredientCount": 2,
+    "missedIngredients": [
+        {
+            "id": 10211111,
+            "amount": 1.0,
+            "unit": "teaspoon",
+            "unitLong": "teaspoon",
+            "unitShort": "tsp",
+            "aisle": "Spices and Seasonings",
+            "name": "sumac powder",
+            "original": "1 teaspoon of sumac powder",
+            "originalName": "sumac powder",
+            "meta": [],
+            "image": "https://img.spoonacular.com/ingredients_100x100/dried-sumac.jpg"
+        },
+        {
+            "id": 99226,
+            "amount": 1.0,
+            "unit": "handful",
+            "unitLong": "handful",
+            "unitShort": "handful",
+            "aisle": "Spices and Seasonings",
+            "name": "sage and mint leaves",
+            "original": "A handful of sage and mint leaves, finely chopped",
+            "originalName": "sage and mint leaves, finely chopped",
+            "meta": [
+                "finely chopped"
+            ],
+            "extendedName": "fresh sage and mint leaves",
+            "image": "https://img.spoonacular.com/ingredients_100x100/fresh-sage.png"
+        }
+    ],
+    "usedIngredients": [
+        {
+            "id": 11527,
+            "amount": 1.0,
+            "unit": "large",
+            "unitLong": "large",
+            "unitShort": "large",
+            "aisle": "Produce",
+            "name": "tomato",
+            "original": "1 large green tomato",
+            "originalName": "green tomato",
+            "meta": [
+                "green"
+            ],
+            "extendedName": "green tomato",
+            "image": "https://img.spoonacular.com/ingredients_100x100/green-tomato.png"
+        }
+    ],
+    "unusedIngredients": [],
+    "likes": 1
+};
